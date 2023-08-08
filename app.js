197149606333cdaebe600a6d94f29a3fbcfb20f6
Chisconike <chinedue856@gmail.com>
2023-08-08 10:10:31 +0000

    His Coming Bible College

---
+++ app.js
@@ -0,0 +1,10 @@
+let toggler = document.getElementById('click');
+toggler.addEventListener('click', function () {
+    let dropDownMenu = document.getElementById('click')
+    if (dropDownMenu.className === 'topnav') {
+        dropDownMenu.className += ' responsive';
+    } else {
+        dropDownMenu.className = 'topnav';
+    }
+});
+
